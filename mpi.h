766240113a22e387ffda035a22ec46136d4fd0d4
Jeff Hammond <jeff.science@gmail.com>
2023-01-31 09:34:34 +0200

    a bunch of defs

--- mpi.h
+++ mpi.h
@@ -1,12 +1,16 @@
-typedef struct __MPI_Comm__     * MPI_Comm;
-typedef struct __MPI_Datatype__ * MPI_Datatype;
-typedef struct __MPI_File__     * MPI_File;
-typedef struct __MPI_Group__    * MPI_Group;
-typedef struct __MPI_Info__     * MPI_Info;
-typedef struct __MPI_Message__  * MPI_Message;
-typedef struct __MPI_Op__       * MPI_Op;
-typedef struct __MPI_Request__  * MPI_Request;
-typedef struct __MPI_Win__      * MPI_Win;
+#include <stddef.h>
+
+typedef struct __MPI_Comm__         * MPI_Comm;
+typedef struct __MPI_Datatype__     * MPI_Datatype;
+typedef struct __MPI_Errhandler__   * MPI_Errhandler;
+typedef struct __MPI_File__         * MPI_File;
+typedef struct __MPI_Group__        * MPI_Group;
+typedef struct __MPI_Info__         * MPI_Info;
+typedef struct __MPI_Message__      * MPI_Message;
+typedef struct __MPI_Op__           * MPI_Op;
+typedef struct __MPI_Request__      * MPI_Request;
+typedef struct __MPI_Session__      * MPI_Session;
+typedef struct __MPI_Win__          * MPI_Win;
 
 typedef struct __MPI_Status__
 {
@@ -17,17 +21,96 @@ typedef struct __MPI_Status__
 }
 MPI_Status;
 
-int MPI_Init(int*,char***);
+typedef ptrdiff_t MPI_Aint;
+typedef ptrdiff_t MPI_Count;
+typedef ptrdiff_t MPI_Offset;
+
+// A.1.3 Prototype Definitions
+typedef void MPI_User_function(void *invec, void *inoutvec, int *len, MPI_Datatype *datatype);
+typedef void MPI_User_function_c(void *invec, void *inoutvec, MPI_Count *len, MPI_Datatype *datatype);
+typedef int MPI_Comm_copy_attr_function(MPI_Comm oldcomm, int comm_keyval, void *extra_state, void *attribute_val_in, void *attribute_val_out, int *flag);
+typedef int MPI_Comm_delete_attr_function(MPI_Comm comm, int comm_keyval, void *attribute_val, void *extra_state);
+typedef int MPI_Win_copy_attr_function(MPI_Win oldwin, int win_keyval, void *extra_state, void *attribute_val_in, void *attribute_val_out, int *flag);
+typedef int MPI_Win_delete_attr_function(MPI_Win win, int win_keyval, void *attribute_val, void *extra_state);
+typedef int MPI_Type_copy_attr_function(MPI_Datatype oldtype, int type_keyval, void *extra_state, void *attribute_val_in, void *attribute_val_out, int *flag);
+typedef int MPI_Type_delete_attr_function(MPI_Datatype datatype, int type_keyval, void *attribute_val, void *extra_state);
+typedef void MPI_Comm_errhandler_function(MPI_Comm *comm, int *error_code, ...);
+typedef void MPI_Win_errhandler_function(MPI_Win *win, int *error_code, ...);
+typedef void MPI_File_errhandler_function(MPI_File *file, int *error_code, ...);
+typedef void MPI_Session_errhandler_function(MPI_Session *session, int *error_code, ...);
+typedef int MPI_Grequest_query_function(void *extra_state, MPI_Status *status);
+typedef int MPI_Grequest_free_function(void *extra_state);
+typedef int MPI_Grequest_cancel_function(void *extra_state, int complete);
+typedef int MPI_Datarep_extent_function(MPI_Datatype datatype, MPI_Aint *extent, void *extra_state);
+typedef int MPI_Datarep_conversion_function(void *userbuf, MPI_Datatype datatype, int count, void *filebuf, MPI_Offset position, void *extra_state);
+typedef int MPI_Datarep_conversion_function_c(void *userbuf, MPI_Datatype datatype, MPI_Count count, void *filebuf, MPI_Offset position, void *extra_state);
+//typedef void MPI_T_event_cb_function(MPI_T_event_instance event_instance, MPI_T_event_registration event_registration, MPI_T_cb_safety cb_safety, void *user_data);
+//typedef void MPI_T_event_free_cb_function(MPI_T_event_registration event_registration, MPI_T_cb_safety cb_safety, void *user_data);
+//typedef void MPI_T_event_dropped_cb_function(MPI_Count count, MPI_T_event_registration event_registration, int source_index, MPI_T_cb_safety cb_safety, void *user_data);
+
+// A.3.7 MPI Environmental Management C Bindings
+int MPI_Add_error_class(int *errorclass);
+int MPI_Add_error_code(int errorclass, int *errorcode);
+int MPI_Add_error_string(int errorcode, const char *string);
+int MPI_Alloc_mem(MPI_Aint size, MPI_Info info, void *baseptr);
+int MPI_Comm_call_errhandler(MPI_Comm comm, int errorcode);
+int MPI_Comm_create_errhandler(MPI_Comm_errhandler_function *comm_errhandler_fn, MPI_Errhandler *errhandler);
+int MPI_Comm_get_errhandler(MPI_Comm comm, MPI_Errhandler *errhandler);
+int MPI_Comm_set_errhandler(MPI_Comm comm, MPI_Errhandler errhandler);
+int MPI_Errhandler_free(MPI_Errhandler *errhandler);
+int MPI_Error_class(int errorcode, int *errorclass);
+int MPI_Error_string(int errorcode, char *string, int *resultlen);
+int MPI_File_call_errhandler(MPI_File fh, int errorcode);
+int MPI_File_create_errhandler(MPI_File_errhandler_function *file_errhandler_fn, MPI_Errhandler *errhandler);
+int MPI_File_get_errhandler(MPI_File file, MPI_Errhandler *errhandler);
+int MPI_File_set_errhandler(MPI_File file, MPI_Errhandler errhandler);
+int MPI_Free_mem(void *base);
+int MPI_Get_library_version(char *version, int *resultlen);
+int MPI_Get_processor_name(char *name, int *resultlen);
+int MPI_Get_version(int *version, int *subversion);
+int MPI_Session_call_errhandler(MPI_Session session, int errorcode);
+int MPI_Session_create_errhandler(MPI_Session_errhandler_function *session_errhandler_fn, MPI_Errhandler *errhandler);
+int MPI_Session_get_errhandler(MPI_Session session, MPI_Errhandler *errhandler);
+int MPI_Session_set_errhandler(MPI_Session session, MPI_Errhandler errhandler);
+int MPI_Win_call_errhandler(MPI_Win win, int errorcode);
+int MPI_Win_create_errhandler(MPI_Win_errhandler_function *win_errhandler_fn, MPI_Errhandler *errhandler);
+int MPI_Win_get_errhandler(MPI_Win win, MPI_Errhandler *errhandler);
+int MPI_Win_set_errhandler(MPI_Win win, MPI_Errhandler errhandler);
+int MPI_Info_create(MPI_Info *info);
+double MPI_Wtime(void);
+double MPI_Wtick(void);
+
+// A.3.9 Process Creation and Management C Bindings
+int MPI_Close_port(const char *port_name);
+int MPI_Comm_disconnect(MPI_Comm *comm);
+int MPI_Info_dup(MPI_Info info, MPI_Info *newinfo);
+int MPI_Info_free(MPI_Info *info);
+int MPI_Info_get_nkeys(MPI_Info info, int *nkeys);
+int MPI_Info_get_nthkey(MPI_Info info, int n, char *key);
+int MPI_Info_get_string(MPI_Info info, const char *key, int *buflen, char *value, int *flag);
+int MPI_Info_set(MPI_Info info, const char *key, const char *value);
+int MPI_Abort(MPI_Comm comm, int errorcode);
+int MPI_Comm_accept(const char *port_name, MPI_Info info, int root, MPI_Comm comm, MPI_Comm *newcomm);
+int MPI_Comm_connect(const char *port_name, MPI_Info info, int root, MPI_Comm comm, MPI_Comm *newcomm);
+int MPI_Comm_get_parent(MPI_Comm *parent);
+int MPI_Comm_join(int fd, MPI_Comm *intercomm);
+int MPI_Comm_spawn(const char *command, char *argv[], int maxprocs, MPI_Info info, int root, MPI_Comm comm, MPI_Comm *intercomm, int array_of_errcodes[]);
+int MPI_Comm_spawn_multiple(int count, char *array_of_commands[], char **array_of_argv[], const int array_of_maxprocs[], const MPI_Info array_of_info[], int root, MPI_Comm comm, MPI_Comm *intercomm, int array_of_errcodes[]);
 int MPI_Finalize(void);
-int MPI_Abort(MPI_Comm,int);
+int MPI_Finalized(int *flag);
+int MPI_Init(int *argc, char ***argv);
+int MPI_Init_thread(int *argc, char ***argv, int required, int *provided);
+int MPI_Initialized(int *flag);
+int MPI_Is_thread_main(int *flag);
+int MPI_Lookup_name(const char *service_name, MPI_Info info, char *port_name);
+int MPI_Open_port(MPI_Info info, char *port_name);
+int MPI_Publish_name(const char *service_name, MPI_Info info, const char *port_name);
+int MPI_Query_thread(int *provided);
+int MPI_Session_finalize(MPI_Session *session);
+int MPI_Session_get_nth_pset(MPI_Session session, MPI_Info info, int n, int *pset_len, char *pset_name);
+int MPI_Session_get_info(MPI_Session session, MPI_Info *info_used);
+int MPI_Session_get_num_psets(MPI_Session session, MPI_Info info, int *npset_names);
+int MPI_Session_get_pset_info(MPI_Session session, const char *pset_name, MPI_Info *info);
+int MPI_Session_init(MPI_Info info, MPI_Errhandler errhandler, MPI_Session *session);
+int MPI_Unpublish_name(const char *service_name, MPI_Info info, const char *port_name);
 
-int MPI_Test(MPI_Request*,int*,MPI_Status*);
-int MPI_Testall(int,MPI_Request[],int*,MPI_Status[]);
-int MPI_Testany(int,MPI_Request[],int*,int*,MPI_Status*);
-int MPI_Testsome(int,MPI_Request[],int*,int[],MPI_Status[]);
-int MPI_Wait(MPI_Request*,MPI_Status*);
-int MPI_Waitall(int,MPI_Request[],MPI_Status[]);
-int MPI_Waitany(int,MPI_Request[],int*,MPI_Status*);
-int MPI_Waitsome(int,MPI_Request[],int*,int[],MPI_Status[]);
-
-int MPI_Test_cancelled(const MPI_Status*,int*);
