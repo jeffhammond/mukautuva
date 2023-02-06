852b07d5e50a80323f564288882386dd44c8853e
Jeff Hammond <jeff.science@gmail.com>
2023-02-06 18:44:54 +0200

    fix Waitany is wrong

--- testreqs.c
+++ testreqs.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdint.h>
+#ifdef NOWRAP
+#include <mpi.h>
+#else
 #include "mpi.h"
+#endif
 
 int main(int argc, char* argv[])
 {
@@ -16,7 +20,24 @@ int main(int argc, char* argv[])
         MPI_Request r;
         MPI_Ibarrier(MPI_COMM_WORLD,&r);
         MPI_Wait(&r,MPI_STATUS_IGNORE);
+
+        MPI_Ibarrier(MPI_COMM_WORLD,&r);
+        MPI_Waitall(1,&r,MPI_STATUS_IGNORE);
+
+        // this does not set index properly
+        int index = 1<<30;
+        MPI_Ibarrier(MPI_COMM_WORLD,&r);
+        MPI_Waitany(1,&r,&index,MPI_STATUS_IGNORE);
+        printf("index=%d\n", index);
+
+        int flag = 0;
+        MPI_Ibarrier(MPI_COMM_WORLD,&r);
+        while (!flag) {
+            MPI_Test(&r,&flag,MPI_STATUS_IGNORE);
+        }
     }
+
+    if (me==0) printf("all done\n");
 
     rc = MPI_Finalize();
 
