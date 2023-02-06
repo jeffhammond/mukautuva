2fc82f9bfba1ddeb7a0825be8dcfcfd2746a589b
Jeff Hammond <jeff.science@gmail.com>
2023-02-06 18:37:01 +0200

    fix status ignore

---
+++ testreqs.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdint.h>
+#include "mpi.h"
+
+int main(int argc, char* argv[])
+{
+    int rc;
+    rc = MPI_Init(&argc,&argv);
+
+    int me, np;
+    MPI_Comm_rank(MPI_COMM_WORLD,&me);
+    MPI_Comm_size(MPI_COMM_WORLD,&np);
+    printf("i am %d of %d\n", me, np);
+
+    {
+        MPI_Request r;
+        MPI_Ibarrier(MPI_COMM_WORLD,&r);
+        MPI_Wait(&r,MPI_STATUS_IGNORE);
+    }
+
+    rc = MPI_Finalize();
+
+    return rc;
+}
