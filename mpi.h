72c771382a5b7007da4b85f69dff6092a4f6df3e
Jeff Hammond <jeff.science@gmail.com>
2023-02-06 14:05:55 +0200

    add const to predefined handles so they cannot be written to

--- mpi.h
+++ mpi.h
@@ -54,69 +54,69 @@ typedef int MPI_Delete_function(MPI_Comm comm, int keyval, void *attribute_val, 
 
 // predefined handles
 
-extern MPI_Request MPI_REQUEST_NULL;
+extern const MPI_Request MPI_REQUEST_NULL;
 
-extern MPI_Errhandler MPI_ERRHANDLER_NULL;
+extern const MPI_Errhandler MPI_ERRHANDLER_NULL;
 
-extern MPI_Info MPI_INFO_NULL;
+extern const MPI_Info MPI_INFO_NULL;
 
-extern MPI_Win MPI_WIN_NULL;
+extern const MPI_Win MPI_WIN_NULL;
 
-extern MPI_File MPI_FILE_NULL;
+extern const MPI_File MPI_FILE_NULL;
 
-extern MPI_Comm MPI_COMM_NULL;
-extern MPI_Comm MPI_COMM_WORLD;
-extern MPI_Comm MPI_COMM_SELF;
+extern const MPI_Comm MPI_COMM_NULL;
+extern const MPI_Comm MPI_COMM_WORLD;
+extern const MPI_Comm MPI_COMM_SELF;
 
-extern MPI_Group MPI_GROUP_NULL;
-extern MPI_Group MPI_GROUP_EMPTY;
+extern const MPI_Group MPI_GROUP_NULL;
+extern const MPI_Group MPI_GROUP_EMPTY;
 
-extern MPI_Message MPI_MESSAGE_NULL;
-extern MPI_Message MPI_MESSAGE_NO_PROC;
+extern const MPI_Message MPI_MESSAGE_NULL;
+extern const MPI_Message MPI_MESSAGE_NO_PROC;
 
-extern MPI_Op MPI_OP_NULL;
-extern MPI_Op MPI_MAX;
-extern MPI_Op MPI_MIN;
-extern MPI_Op MPI_SUM;
-extern MPI_Op MPI_PROD;
-extern MPI_Op MPI_LAND;
-extern MPI_Op MPI_BAND;
-extern MPI_Op MPI_LOR;
-extern MPI_Op MPI_BOR;
-extern MPI_Op MPI_LXOR;
-extern MPI_Op MPI_BXOR;
-extern MPI_Op MPI_MAXLOC;
-extern MPI_Op MPI_MINLOC;
-extern MPI_Op MPI_REPLACE;
-extern MPI_Op MPI_NO_OP;
+extern const MPI_Op MPI_OP_NULL;
+extern const MPI_Op MPI_MAX;
+extern const MPI_Op MPI_MIN;
+extern const MPI_Op MPI_SUM;
+extern const MPI_Op MPI_PROD;
+extern const MPI_Op MPI_LAND;
+extern const MPI_Op MPI_BAND;
+extern const MPI_Op MPI_LOR;
+extern const MPI_Op MPI_BOR;
+extern const MPI_Op MPI_LXOR;
+extern const MPI_Op MPI_BXOR;
+extern const MPI_Op MPI_MAXLOC;
+extern const MPI_Op MPI_MINLOC;
+extern const MPI_Op MPI_REPLACE;
+extern const MPI_Op MPI_NO_OP;
 
-extern MPI_Datatype MPI_DATATYPE_NULL;
-extern MPI_Datatype MPI_BYTE;
-extern MPI_Datatype MPI_PACKED;
-extern MPI_Datatype MPI_CHAR;
-extern MPI_Datatype MPI_SHORT;
-extern MPI_Datatype MPI_INT;
-extern MPI_Datatype MPI_LONG;
-extern MPI_Datatype MPI_FLOAT;
-extern MPI_Datatype MPI_DOUBLE;
-extern MPI_Datatype MPI_LONG_DOUBLE;
-extern MPI_Datatype MPI_UNSIGNED_CHAR;
-extern MPI_Datatype MPI_SIGNED_CHAR;
-extern MPI_Datatype MPI_UNSIGNED_SHORT;
-extern MPI_Datatype MPI_UNSIGNED_LONG;
-extern MPI_Datatype MPI_UNSIGNED;
-extern MPI_Datatype MPI_FLOAT_INT;
-extern MPI_Datatype MPI_DOUBLE_INT;
-extern MPI_Datatype MPI_LONG_DOUBLE_INT;
-extern MPI_Datatype MPI_LONG_INT;
-extern MPI_Datatype MPI_SHORT_INT;
-extern MPI_Datatype MPI_2INT;
-extern MPI_Datatype MPI_WCHAR;
-extern MPI_Datatype MPI_LONG_LONG_INT;
-extern MPI_Datatype MPI_LONG_LONG;
-extern MPI_Datatype MPI_UNSIGNED_LONG_LONG;
-extern MPI_Datatype MPI_2COMPLEX;
-extern MPI_Datatype MPI_2DOUBLE_COMPLEX;
+extern const MPI_Datatype MPI_DATATYPE_NULL;
+extern const MPI_Datatype MPI_BYTE;
+extern const MPI_Datatype MPI_PACKED;
+extern const MPI_Datatype MPI_CHAR;
+extern const MPI_Datatype MPI_SHORT;
+extern const MPI_Datatype MPI_INT;
+extern const MPI_Datatype MPI_LONG;
+extern const MPI_Datatype MPI_FLOAT;
+extern const MPI_Datatype MPI_DOUBLE;
+extern const MPI_Datatype MPI_LONG_DOUBLE;
+extern const MPI_Datatype MPI_UNSIGNED_CHAR;
+extern const MPI_Datatype MPI_SIGNED_CHAR;
+extern const MPI_Datatype MPI_UNSIGNED_SHORT;
+extern const MPI_Datatype MPI_UNSIGNED_LONG;
+extern const MPI_Datatype MPI_UNSIGNED;
+extern const MPI_Datatype MPI_FLOAT_INT;
+extern const MPI_Datatype MPI_DOUBLE_INT;
+extern const MPI_Datatype MPI_LONG_DOUBLE_INT;
+extern const MPI_Datatype MPI_LONG_INT;
+extern const MPI_Datatype MPI_SHORT_INT;
+extern const MPI_Datatype MPI_2INT;
+extern const MPI_Datatype MPI_WCHAR;
+extern const MPI_Datatype MPI_LONG_LONG_INT;
+extern const MPI_Datatype MPI_LONG_LONG;
+extern const MPI_Datatype MPI_UNSIGNED_LONG_LONG;
+extern const MPI_Datatype MPI_2COMPLEX;
+extern const MPI_Datatype MPI_2DOUBLE_COMPLEX;
 
 // functions
 
