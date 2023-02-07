541cbc93066740d474cfad27b4aa57c22365a32f
Jeff Hammond <jeff.science@gmail.com>
2023-02-07 22:39:24 +0200

    add max values for compile-time constants from https://github.com/mpiwg-abi/abi-issues/issues/1

--- mpi.h
+++ mpi.h
@@ -2,3 +2,14 @@
 #define MUK_EXTERN extern
 #include "mpi-predefined.h"
 #include "mpi-prototypes.h"
+
+#define MPI_MAX_PROCESSOR_NAME          256
+#define MPI_MAX_LIBRARY_VERSION_STRING 8192
+#define MPI_MAX_ERROR_STRING            512
+#define MPI_MAX_DATAREP_STRING          128
+#define MPI_MAX_PORT_NAME              1024
+#define MPI_MAX_OBJECT_NAME             128
+#define MPI_MAX_STRINGTAG_LEN           256
+#define MPI_MAX_PSET_NAME_LEN           256
+#define MPI_MAX_INFO_KEY                255
+#define MPI_MAX_INFO_VAL               1024
