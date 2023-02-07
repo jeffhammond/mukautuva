d65b39eadee8f0b1d77d8544bea6ee801dffd612
Jeff Hammond <jeff.science@gmail.com>
2023-02-07 09:37:49 +0200

    cleanup test

--- testreqs.c
+++ testreqs.c
@@ -18,7 +18,7 @@ int main(int argc, char* argv[])
     int me, np;
     MPI_Comm_rank(MPI_COMM_WORLD,&me);
     MPI_Comm_size(MPI_COMM_WORLD,&np);
-    printf("i am %d of %d\n", me, np);
+    printf("I am %d of %d\n", me, np);
     fflush(0);
     MPI_Barrier(MPI_COMM_WORLD);
     usleep(1);
@@ -56,7 +56,7 @@ int main(int argc, char* argv[])
     {
         MPI_Barrier(MPI_COMM_WORLD);
         fflush(0);
-        if (me==0) printf("Isend+Recv(s)+Waitall\n");
+        if (me==0) printf("Isend+Recv(s)+Wait\n");
         fflush(0);
 
         int buffer[2] = { me };
@@ -67,10 +67,13 @@ int main(int argc, char* argv[])
         MPI_Recv(&buffer[1], 1, MPI_INT, me, 99, MPI_COMM_WORLD, &s);
         MPI_Wait(&r,MPI_STATUS_IGNORE);
 
-        int rcount = -1;
+        int rcount = -3;
         MPI_Get_count(&s, MPI_INT, &rcount);
-        printf("[%d]: SOURCE=%d TAG=%d ERROR=%d count=%d\n",
-                me, s.MPI_SOURCE, s.MPI_TAG, s.MPI_ERROR, rcount);
+        if ((s.MPI_SOURCE != me) || (s.MPI_TAG != 99) || (rcount != 1)) {
+            printf("[%d]: SOURCE=%d TAG=%d count=%d\n",
+                    me, s.MPI_SOURCE, s.MPI_TAG, rcount);
+            MPI_Abort(MPI_COMM_WORLD,1);
+        }
     }
 
     if (1)
@@ -94,10 +97,13 @@ int main(int argc, char* argv[])
         // functions that take a single MPI_Status argument, the error code is returned
         // by the function, and the value of the MPI_ERROR field in the MPI_Status argument
         // is undefined (see 3.2.5).
-        int rcount = -1;
+        int rcount = -3;
         MPI_Get_count(&s, MPI_INT, &rcount);
-        printf("[%d]: SOURCE=%d TAG=%d count=%d\n",
-                me, s.MPI_SOURCE, s.MPI_TAG, rcount);
+        if ((s.MPI_SOURCE != me) || (s.MPI_TAG != 99) || (rcount != 1)) {
+            printf("[%d]: SOURCE=%d TAG=%d count=%d\n",
+                    me, s.MPI_SOURCE, s.MPI_TAG, rcount);
+            MPI_Abort(MPI_COMM_WORLD,1);
+        }
     }
 
     if (1)
@@ -122,8 +128,11 @@ int main(int argc, char* argv[])
         // is undefined (see 3.2.5).
         int rcount = -3;
         MPI_Get_count(&s[1], MPI_INT, &rcount);
-        printf("[%d]: SOURCE=%d TAG=%d ERROR=%d count=%d\n",
-                me, s[1].MPI_SOURCE, s[1].MPI_TAG, s[1].MPI_ERROR, rcount);
+        if ((s[1].MPI_SOURCE != me) || (s[1].MPI_TAG != 99) || (s[1].MPI_ERROR != 0) || (rcount != 1)) {
+            printf("[%d]: SOURCE=%d TAG=%d ERROR=%d count=%d\n",
+                    me, s[1].MPI_SOURCE, s[1].MPI_TAG, s[1].MPI_ERROR, rcount);
+            MPI_Abort(MPI_COMM_WORLD,1);
+        }
     }
 
     {
