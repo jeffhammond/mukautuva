7ad58f0404c2f8dcf68c24076924e665c67210f3
Jeff Hammond <jeff.science@gmail.com>
2023-02-08 17:43:16 +0200

    tester updates

--- test.sh
+++ test.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+NP=4
+OPTS="--mca osc ucx"
+
 make -j && \
-MPI_LIB=/usr/lib/x86_64-linux-gnu/libmpi.so mpirun.openmpi -n 2 $1 || \
-MPI_LIB=/usr/lib/x86_64-linux-gnu/libmpi.so mpirun.openmpi -n 2 gdb -ex "set width 1000" -ex "thread apply all bt" -ex run -ex bt -ex "set confirm off" -ex quit --args  $1 ; \
-MPI_LIB=/usr/lib/x86_64-linux-gnu/libmpich.so mpirun.mpich -n 2 $1 || \
-MPI_LIB=/usr/lib/x86_64-linux-gnu/libmpich.so mpirun.mpich -n 2 gdb -ex "set width 1000" -ex "thread apply all bt" -ex run -ex bt -ex "set confirm off" -ex quit --args $1
+MPI_LIB=/usr/lib/x86_64-linux-gnu/libmpi.so mpirun.openmpi ${OPTS} -n ${NP} $1 || \
+MPI_LIB=/usr/lib/x86_64-linux-gnu/libmpi.so mpirun.openmpi ${OPTS} -n ${NP} gdb -ex "set width 1000" -ex "thread apply all bt" -ex run -ex bt -ex "set confirm off" -ex quit --args  $1 ; \
+MPI_LIB=/usr/lib/x86_64-linux-gnu/libmpich.so mpirun.mpich -n ${NP} $1 || \
+MPI_LIB=/usr/lib/x86_64-linux-gnu/libmpich.so mpirun.mpich -n ${NP} gdb -ex "set width 1000" -ex "thread apply all bt" -ex run -ex bt -ex "set confirm off" -ex quit --args $1
