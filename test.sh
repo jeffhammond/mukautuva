c4164529380bda46cdef2da70c05ce9b183caefc
Jeff Hammond <jeff.science@gmail.com>
2023-02-09 14:23:44 +0200

    better

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@
 NP=4
 OPTS="--mca osc ucx"
 
-make -j && \
+make -j $1 && \
 MPI_LIB=/usr/lib/x86_64-linux-gnu/libmpi.so mpirun.openmpi ${OPTS} -n ${NP} $1 || \
 MPI_LIB=/usr/lib/x86_64-linux-gnu/libmpi.so mpirun.openmpi ${OPTS} -n ${NP} gdb -ex "set width 1000" -ex "thread apply all bt" -ex run -ex bt -ex "set confirm off" -ex quit --args  $1 ; \
 MPI_LIB=/usr/lib/x86_64-linux-gnu/libmpich.so mpirun.mpich -n ${NP} $1 || \
