5a1625b35995878445b4f081c0c139a83c080f30
Jeff Hammond <jeff.science@gmail.com>
2023-02-15 21:10:13 +0200

    optimize the branching

--- impl-functions.c
+++ impl-functions.c
@@ -1368,11 +1368,26 @@ void WRAP_Finalize_handle_key(void)
 
 static bool IS_PREDEFINED_OP(MPI_Op op)
 {
-    return (op == MPI_MAX || op == MPI_MIN || op == MPI_SUM || op == MPI_PROD ||
-            op == MPI_LAND || op == MPI_LOR || op == MPI_LXOR ||
-            op == MPI_BAND || op == MPI_BOR || op == MPI_BXOR ||
-            op == MPI_MAXLOC || op == MPI_MINLOC ||
-            op == MPI_REPLACE || op == MPI_NO_OP);
+    // it is ideal to optimize the branching order here by ordering the tests
+    // in order of the frequency that the predefine ops are used in real code.
+    return (op == MPI_SUM ||
+            op == MPI_MAX ||
+            op == MPI_MIN ||
+            op == MPI_LAND ||
+            op == MPI_LOR ||
+            op == MPI_BAND ||
+            op == MPI_BOR ||
+            op == MPI_LXOR ||
+            op == MPI_BXOR ||
+            op == MPI_MAXLOC ||
+            op == MPI_MINLOC ||
+            // no one uses this, so it is last among the valid ones.
+            op == MPI_PROD ||
+            // these two are invalid for reductions but they are predefined
+            // so we will include them and let the implementation throw an
+            // error in the reduction.
+            op == MPI_REPLACE ||
+            op == MPI_NO_OP);
 }
 
 typedef void WRAP_User_function(void *invec, void *inoutvec, int *len, MPI_Datatype ** datatype);
