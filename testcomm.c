826b3aa2f84e5580c263e5f1a8e6aca7ca76df09
Jeff Hammond <jeff.science@gmail.com>
2023-02-17 18:54:58 +0200

    add test that comm_split returns null when it should

--- testcomm.c
+++ testcomm.c
@@ -23,19 +23,19 @@ int main(int argc, char* argv[])
     MPI_Comm_size(MPI_COMM_WORLD,&np);
     printf("I am %d of %d\n", me, np);
 
-    int result;
-    MPI_Comm world = MPI_COMM_WORLD;
-    MPI_Comm_compare(MPI_COMM_WORLD,world,&result);
-    if (result != MPI_IDENT) MPI_Abort(MPI_COMM_WORLD,result);
-    
-    MPI_Comm dup;
-    MPI_Comm_dup(MPI_COMM_WORLD,&dup);
-    MPI_Barrier(dup);
-
-    MPI_Comm_compare(MPI_COMM_WORLD,dup,&result);
-    if (result != MPI_CONGRUENT) MPI_Abort(MPI_COMM_WORLD,result);
-
     {
+        int result;
+        MPI_Comm world = MPI_COMM_WORLD;
+        MPI_Comm_compare(MPI_COMM_WORLD,world,&result);
+        if (result != MPI_IDENT) MPI_Abort(MPI_COMM_WORLD,result);
+
+        MPI_Comm dup;
+        MPI_Comm_dup(MPI_COMM_WORLD,&dup);
+        MPI_Barrier(dup);
+
+        MPI_Comm_compare(MPI_COMM_WORLD,dup,&result);
+        if (result != MPI_CONGRUENT) MPI_Abort(MPI_COMM_WORLD,result);
+
         MPI_Comm split;
         MPI_Comm_split(MPI_COMM_WORLD,0,-me,&split);
         MPI_Barrier(split);
