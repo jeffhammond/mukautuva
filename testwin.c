b14fbabb37d6a8f26d42493630967681c91ba84a
Jeff Hammond <jeff.science@gmail.com>
2023-02-17 18:54:34 +0200

    remove unnecessary io

--- testwin.c
+++ testwin.c
@@ -16,8 +16,6 @@ int main(int argc, char* argv[])
     int rc;
     rc = MPI_Init(&argc,&argv);
 
-    printf("main: MPI_COMM_WORLD=%lx\n", (intptr_t)MPI_COMM_WORLD);
-
     int me, np;
     MPI_Comm_rank(MPI_COMM_WORLD,&me);
     MPI_Comm_size(MPI_COMM_WORLD,&np);
