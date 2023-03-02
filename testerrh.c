82c7e3bc9f24cbc1677e3618923a46ad2a15b5b6
Jeff Hammond <jeff.science@gmail.com>
2023-03-02 17:40:51 +0200

    errh test

--- testerrh.c
+++ testerrh.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 
 #ifdef NOWRAP
 #include <mpi.h>
@@ -9,7 +10,7 @@
 
 void callback(MPI_Comm * comm, int *err, ...)
 {
-    printf("callback\n");
+    printf("callback *comm=%lx *err=%d\n",(intptr_t)*comm,*err);
     return;
 }
 
@@ -19,35 +20,35 @@ int main(int argc, char *argv[])
 
     MPI_Errhandler errh;
     MPI_Comm_create_errhandler(callback, &errh);
-    printf("C errh=%x\n",errh);
+    printf("C errh=%lx\n",(intptr_t)errh);
 
     MPI_Comm_set_errhandler(MPI_COMM_WORLD, errh);
-    printf("S errh=%x\n",errh);
+    printf("S errh=%lx\n",(intptr_t)errh);
 
     MPI_Comm dup;
     MPI_Comm_dup(MPI_COMM_WORLD,&dup);
 
     MPI_Errhandler temp0;
     MPI_Comm_get_errhandler(MPI_COMM_WORLD,&temp0);
-    printf("G temp0=%x\n",temp0);
+    printf("G temp0=%lx\n",(intptr_t)temp0);
 
     MPI_Errhandler temp1;
     MPI_Comm_get_errhandler(dup,&temp1);
-    printf("G temp1=%x\n",temp1);
+    printf("G temp1=%lx\n",(intptr_t)temp1);
 
     MPI_Comm_free(&dup);
 
     MPI_Errhandler_free(&temp0);
-    printf("F temp0=%x\n",temp0);
+    printf("F temp0=%lx\n",(intptr_t)temp0);
 
     MPI_Errhandler_free(&temp1);
-    printf("F temp1=%x\n",temp1);
+    printf("F temp1=%lx\n",(intptr_t)temp1);
 
     MPI_Errhandler_free(&errh);
-    printf("F errh=%x\n",errh);
+    printf("F errh=%lx\n",(intptr_t)errh);
 
     MPI_Errhandler_free(&errh);
-    printf("F errh=%x\n",errh);
+    printf("F errh=%lx\n",(intptr_t)errh);
 
     MPI_Finalize();
     return 0;
