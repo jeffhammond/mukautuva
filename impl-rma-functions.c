3165b4a968aa527f75c289a4ecc06391f7787734
Jeff Hammond <jeff.science@gmail.com>
2023-03-02 21:57:06 +0200

    silently ignore interesting cases of win_create_keyval

--- impl-rma-functions.c
+++ impl-rma-functions.c
@@ -99,25 +99,29 @@ int WRAP_Win_call_errhandler(WRAP_Win win, int errorcode)
 
 // attribute stuff
 
-#if 0
 int WRAP_Win_create_keyval(WRAP_Win_copy_attr_function *win_copy_attr_fn, WRAP_Win_delete_attr_function *win_delete_attr_fn, int *win_keyval, void *extra_state)
 {
-    MPI_Win_copy_attr_function * impl_win_copy_attr_fn = (MPI_Win_copy_attr_function*)win_copy_attr_fn;
+    MPI_Win_copy_attr_function * impl_win_copy_attr_fn;
     if ((intptr_t)win_copy_attr_fn == (intptr_t)MUK_WIN_NULL_COPY_FN) {
         impl_win_copy_attr_fn = MPI_WIN_NULL_COPY_FN;
-    } else {
-        printf("%s : %d FIXME\n",__func__,__LINE__);
     }
-    MPI_Win_delete_attr_function * impl_win_delete_attr_fn = (MPI_Win_delete_attr_function*)win_delete_attr_fn;
+    else {
+        //printf("%s : %d FIXME\n",__func__,__LINE__);
+        // FIXME with a trampoline
+        impl_win_copy_attr_fn = MPI_WIN_DUP_FN;
+    }
+    MPI_Win_delete_attr_function * impl_win_delete_attr_fn;
     if ((intptr_t)win_delete_attr_fn == (intptr_t)MUK_WIN_NULL_DELETE_FN) {
         impl_win_delete_attr_fn = MPI_WIN_NULL_DELETE_FN;
-    } else {
-        printf("%s : %d FIXME\n",__func__,__LINE__);
+    }
+    else {
+        //printf("%s : %d FIXME\n",__func__,__LINE__);
+        // FIXME with a trampoline
+        impl_win_delete_attr_fn = MPI_WIN_NULL_DELETE_FN;
     }
     int rc = IMPL_Win_create_keyval(impl_win_copy_attr_fn, impl_win_delete_attr_fn, win_keyval, extra_state);
     return RETURN_CODE_IMPL_TO_MUK(rc);
 }
-#endif
 
 int WRAP_Win_free_keyval(int *win_keyval)
 {
